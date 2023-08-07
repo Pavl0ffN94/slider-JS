03f2ef861767a8282ae33e18e1b17412524ac5fb
Николай Павлов <n.pavlov@i-sys.ru>
2023-08-07 18:30:07 +0400

    git commit -m

--- Slider JS/script.js
+++ Slider JS/script.js
@@ -1,115 +1,49 @@
 function SliderJS(options) {
   const slider = document.querySelector(`#${options.id}`);
-  const slides = slider.querySelectorAll('img');
+  const slides = slider.querySelectorAll('carusel img');
   const controlsContainer = document.createElement('div');
+  const carusel = document.createElement('div');
+  carusel.classList.add('carusel');
+  slider.classList.add('slider-container');
+
+  const loop = options.loop;
+  const controls = options.controls;
+  const autoplay = options.autoplay;
 
   let currentSlide = 0;
   let interval;
-  let touchStartX = 0;
 
-  slider.classList.add('slider-container');
-
-  function showSlide(slideIndex) {
-    slides.forEach((slide, index) => {
-      slide.style.opacity = index === slideIndex ? '1' : '0';
-    });
-  }
-
-  slider.addEventListener('touchstart', (event) => {
-    touchStartX = event.touches[0].clientX;
+  slides.forEach(img => {
+    carusel.appendChild(img);
   });
 
-  slider.addEventListener('touchend', (event) => {
-    const touchEndX = event.changedTouches[0].clientX;
-    const touchDiff = touchEndX - touchStartX;
+  slider.appendChild(carusel);
 
-    if (touchDiff > 50) {
-      prevSlide();
-    } else if (touchDiff < -50) {
-      nextSlide();
-    }
-  });
+  let isDragStart = false;
+  let startX = 0;
+  let scrollLeft = 0;
 
-  function nextSlide() {
-    currentSlide = (currentSlide + 1) % slides.length;
-    showSlide(currentSlide);
-  }
+  const dragStart = (e) => {
+    console.log(e);
+    isDragStart = true;
+    startX = e.pageX - carusel.offsetLeft;
+    scrollLeft = carusel.scrollLeft;
+  };
 
-  function prevSlide() {
-    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
-    showSlide(currentSlide);
-  }
+  const dragEnd = () => {
+    isDragStart = false;
+  };
 
-  function startSlider() {
-    interval = setInterval(() => {
-      nextSlide();
-    }, options.options.interval || 3000);
-  }
+  const drag = (e) => {
+    if (!isDragStart) return;
+    const x = e.pageX - carusel.offsetLeft;
+    const walk = (x - startX) * 3;
+    carusel.scrollLeft = scrollLeft - walk;
+  };
 
-  function stopSlider() {
-    clearInterval(interval);
-  }
-
-  function initSlider() {
-    showSlide(currentSlide);
-
-    controlsContainer.classList.add('slider-controls'); 
-    slider.appendChild(controlsContainer)
-
-    if (options.options.controls) {
-      const prevBtn = document.createElement('button');
-      prevBtn.classList.add('btn-prev');
-      prevBtn.textContent = 'Previous';
-      prevBtn.addEventListener('click', () => {
-        prevSlide();
-      });
-
-      const stopShow = document.createElement('button');
-      stopShow.textContent = 'Stop Show';
-      stopShow.addEventListener('click', () => {
-        stopSlider();
-      });
-
-      const startShow = document.createElement('button');
-      startShow.textContent = 'Start Show';
-      startShow.addEventListener('click', () => {
-        startSlider();
-      });
-
-      const nextBtn = document.createElement('button');
-      nextBtn.classList.add('btn-next');
-      nextBtn.textContent = 'Next';
-      nextBtn.addEventListener('click', () => {
-        nextSlide();
-      });
-
-      controlsContainer.appendChild(prevBtn);
-      controlsContainer.appendChild(stopShow);
-      controlsContainer.appendChild(startShow);
-      controlsContainer.appendChild(nextBtn);
-    }
-
-    if (!options.options.hideControls) {
-      const hideControlsBtn = document.createElement('button');
-      hideControlsBtn.textContent = 'Hide Controls';
-      hideControlsBtn.addEventListener('click', () => {
-        const buttons = document.querySelectorAll('button:not(.controls-hiddens)');
-        
-        buttons.forEach(button => {
-          button.style.opacity = button.style.opacity === '0' ? '1' : '0';
-        });
-      });
-
-      const controlsHiddenContainer = document.createElement('div');
-      controlsHiddenContainer.classList.add('controls-hiddens');
-      controlsHiddenContainer.appendChild(hideControlsBtn);
-      slider.appendChild(controlsHiddenContainer);
-    }
-
-    if (options.options.autoplay) {
-      startSlider();
-    }
-  }
-
-  initSlider();
+  carusel.addEventListener('mousedown', dragStart);
+  carusel.addEventListener('mouseup', dragEnd);
+  carusel.addEventListener('mouseleave', dragEnd);
+  carusel.addEventListener('mousemove', drag);
 }
+
