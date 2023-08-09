c2acf398301515f56d17a362cfdf6a256f95980d
Николай Павлов <n.pavlov@i-sys.ru>
2023-08-09 18:04:34 +0400

    change of approach

--- Slider JS/script.js
+++ Slider JS/script.js
@@ -3,167 +3,60 @@ function SliderJS(options) {
   const slides = slider.querySelectorAll('img');
   const carusel = document.createElement('div');
 
-  const controls = options.options.controls;
-  const autoplay = options.options.autoplay;
-  const loop = options.options.loop;
-
-  let interval;
-
   slider.classList.add('slider-container');
   carusel.classList.add('carusel');
 
   slides.forEach(img => {
-    img.classList.add('slide')
+    img.classList.add('slider-item')
     carusel.appendChild(img);
   });
 
   slider.appendChild(carusel);
 
+  const arraySlidesItem = [];
 
-// начало кода для обработки клика мышкой 
-  let isDragStart = false;
-  let startX = 0;
-  let scrollLeft = 0;
-  let currentSlide = 0;
+  slides.forEach((slide, i) => {
+    arraySlidesItem[i] = slide.src;
+    slide.remove();
+});
+console.log(arraySlidesItem);
 
-  const dragStart = e => {
-    e.preventDefault();
-    isDragStart = true;
-    startX = e.pageX - carusel.offsetLeft;
-    scrollLeft = carusel.scrollLeft;
-  };
+  let step = 0;
+  let offset = 0;
+  const slideWidth = slides[0].clientWidth
 
-  const drag = e => {
-    if (!isDragStart) return;
-    const x = e.pageX - carusel.offsetLeft;
-    const walk = (x - startX) * 2;
-    carusel.scrollLeft = scrollLeft - walk;
-  };
-
-  const dragEnd = () => {
-    isDragStart = false;
-  };
-
-  carusel.addEventListener('mousedown', dragStart);
-  carusel.addEventListener('mouseup', dragEnd);
-  carusel.addEventListener('mouseleave', dragEnd);
-  carusel.addEventListener('mousemove', drag); 
-//конец кода для обработки клика мышкой
-
-
-function showSlide(slideIndex) {
-  const slideWidth = slides[0].clientWidth;
-  const containerWidth = carusel.offsetWidth;
-  const slideOffset = slideIndex * slideWidth;
-  const centrOffst = (containerWidth - slideWidth) / 2;
-  const scrollTo = slideOffset - centrOffst;
-  carusel.scrollLeft = scrollTo;
-}
-
-function nextSlide() {
-  currentSlide = (currentSlide + 1) % slides.length;
-  showSlide(currentSlide);
-  if (loop ||currentSlide === 0) {
-    stopSlider();
-  }
-}
-
-function prevSlide() {
-  currentSlide = (currentSlide > 0) ? currentSlide - 1 : slides.length - 1;
-  showSlide(currentSlide);
-  if (loop || currentSlide === slides.length - 1) {
-    stopSlider();
-  }
-}
-
-// код для автоматичской прокрутки слйдера
-  function startSlider() {
-    interval = setInterval(() => {
-      nextPosition();
-      if (currentSlide === slides.length - 1 && !loop) {
-        clearInterval(interval);
-      }
-    }, options.options.interval || 3000);
-  }
-
-   function stopSlider() {
-    clearInterval(interval);
-  }
-// конец кода для автоматичской прокрутки слайдера
-
-
-// кнопки и их обработка 
-  function initSlider() {
-    showSlide(currentSlide);
-    const controlsContainer = document.createElement('div');
-
-    controlsContainer.classList.add('slider-controls');
-    slider.appendChild(controlsContainer);
-
-    if (controls) {
-      const prevBtn = document.createElement('button');
-      const nextBtn = document.createElement('button');
-      const startStopBtn = document.createElement('button');
-      const hideControlsBtn = document.createElement('button');
-      const controlsHiddenContainer = document.createElement('div');
-
-      let isPlaying = false;
-
-      prevBtn.classList.add('btn-prev');
-      nextBtn.classList.add('btn-next');
-      startStopBtn.classList.add('btn-start');
-      hideControlsBtn.classList.add('btn-hide-controls');
-
-      startStopBtn.addEventListener('click', () => {
-        if (isPlaying) {
-          stopSlider();
-          startStopBtn.classList.remove('btn-stop'); 
-          startStopBtn.classList.add('btn-start'); 
-        } else {
-          stopSlider();
-          startSlider();
-          startStopBtn.classList.remove('btn-start'); 
-          startStopBtn.classList.add('btn-stop');
-          isPlaying = true 
-        }
-      });
-    
-      prevBtn.addEventListener('click', () => {
-        if (currentSlide === 0 && !loop) {
-          return;
-        }
-        prevSlide();
-      });
-
-      nextBtn.addEventListener('click', () => {
-        if (currentSlide === slides.length - 1 && !loop) {
-          return;
-        }
-        nextSlide();
-      });
-
-      controlsContainer.appendChild(startStopBtn);
-
-      hideControlsBtn.textContent = 'Hide Controls';
-      hideControlsBtn.addEventListener('click', () => {
-        const buttons = document.querySelectorAll('button:not(.controls-hiddens)');
-        buttons.forEach(button => {
-          button.style.opacity = button.style.opacity === '0' ? '1' : '0';
-        });
-      });
-
-      controlsHiddenContainer.classList.add('controls-hiddens');
-      controlsHiddenContainer.appendChild(hideControlsBtn);
-      slider.appendChild(controlsHiddenContainer);
-      slider.appendChild(nextBtn);
-      slider.appendChild(prevBtn);
+  console.log(innerWidth);
+  function draw() {
+    let img = document.createElement('img');
+    img.src = arraySlidesItem[step];
+    img.classList.add('slider-item');
+    img.style.left = offset * slideWidth + 'px' ;
+    carusel.appendChild(img)
+    if(step + 1 === slides.length){
+      step = 0
+    } else{
+      step++;
     }
-
-    if (autoplay) {
-      startSlider();
-    }
+    offset = 1;
   }
 
-  initSlider(); 
+  function nextSlide(){
+    document.onclick = null
+    const repeatSlides = slider.querySelectorAll('img');
+    let offset2= 0;
+    repeatSlides.forEach((slide) => {
+      slide.style.left = offset2 * slideWidth - 2100 + 'px';
+  });
+  setTimeout(()=>{
+    repeatSlides[0].remove();
+    draw();
+    document.onclick = nextSlide;
+  }, 1000)
+  }
+
+  draw();
+document.onclick = nextSlide;
+
 }
+
  
